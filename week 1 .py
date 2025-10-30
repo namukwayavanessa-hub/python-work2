28005329f02a7809f36fa6c2062e2f3f564789fa
namukwayavanessa <kimoliza321@gmail.com>
2025-10-30 13:15:05 +0300

    added python operators and expresses

---
+++ week 1 .py
@@ -0,0 +1,23 @@
+# . Practice Questions 
+#  A. Short Questions 
+# 1. What is the difference between = and ==? 
+# = = means equal to in comparison operation for example 5==5 while = m
+# 2. What is the output of 10 % 3?
+# let the variables be; 
+a=10
+b=3
+# % is the modulus sign and we only consider the remainder as the answer
+print (a%b) 
+# 3. Write an example using the not operator
+# not reverses the boolean value 
+# example
+v=True
+y=False
+print(not(y)) #giving the results as true
+print(not(v)) #giving the result as false since the answers are given in a reverse format
+# . What will this print be? 
+a=4
+b=2
+print(a**b//b)
+#the results will be 8 this is because;
+a**b 
