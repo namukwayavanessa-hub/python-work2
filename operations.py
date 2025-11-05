903c4a93b5478dbc587529fad822ad2177cbe9c1
namukwayavanessa <kimoliza321@gmail.com>
2025-11-05 17:28:32 +0300

    operations

---
+++ operations.py
@@ -0,0 +1,23 @@
+#what is the output of 10 % 3
+print (10 % 3) #the output  will be 1 because we consider the remainder of 10 % 3
+# what will this print be ?
+a=4
+b=2
+print(a**b//b) # the print will be 8
+#area and perimeter of a rectangle
+length = 8
+widith = 3
+area =length * widith 
+perimeter = 2 * (length + widith )
+print("Area:" , area)
+print("Perimeter:" ,perimeter)
+# check if a number is Even 
+num = int(input( "Enter a number: "))
+print(num % 2 == 0)
+#combine logical operators
+x = 7 
+y =10 
+print( x > 5 and y < 15)
+#temperature check 
+temp = 30
+print( temp >25 and  temp <35)
