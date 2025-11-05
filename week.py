6e7ece9886469d409eb9da2b97a47373c3b11d0a
namukwayavanessa <kimoliza321@gmail.com>
2025-11-05 11:07:36 +0300

    added python operators and express

---
+++ week.py
@@ -0,0 +1,15 @@
+y=3
+x=2
+print(x**y)#for the results its true 
+print ("welcome to Python Programming!")
+print ("five is greater than two !")
+print(str("gorret"))
+age= "25"
+name = input("enter your nane:")
+print("hello," +name)
+a=12
+b=3
+print(a+b)
+print(a*b)
+birthyear =input("enter your birthyear:")
+
